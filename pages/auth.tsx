984c61de2dcf75683c18ae57914da71a02ed949f
mathers08 <black.mathers08@mail.ru>
2023-04-19 18:29:26 +0300

    init

---
+++ pages/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div className="bg-[url('/images/hero.jpg')] relative w-full h-full bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img className='h-12' src="/images/logo.png" alt="Logo" />
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
