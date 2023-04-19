3841fafc2c08d6d70478e56cd22ea243358232ae
mathers08 <black.mathers08@mail.ru>
2023-04-19 19:54:05 +0300

    create auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Input from "../components/Input";
 
 const Auth = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [variant, setVariant] = useState('login');
+
+  const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+  const onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+  }, []);
+
   return (
     <div className="bg-[url('/images/hero.jpg')] relative w-full h-full bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
+      <div className="bg-black w-full h-full md:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img className='h-12' src="/images/logo.png" alt="Logo" />
+          <img className="h-12" src="/images/logo.png" alt="Logo" />
         </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 p-16 self-center mt-2 md:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Register'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === 'register' && (
+                <Input id="username" onChange={onUsernameChange} label="Username" value={username} />
+              )}
+              <Input id="email" onChange={onEmailChange} label="Email" value={email} type="email" />
+              <Input id="password" onChange={onPasswordChange} label="Password" value={password} type="password" />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+              <span onClick={toggleVariant} className="text-white ml-2 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Create an account' : 'Login'}
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
